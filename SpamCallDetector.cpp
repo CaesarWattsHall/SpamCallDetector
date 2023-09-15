5842832a2b105d04d17460845b562764cd2818c3
Caesar R. Watts-Hall <93171607+CaesarWattsHall@users.noreply.github.com>
2023-09-15 05:29:18 -0500

    Update SpamCallDetector.cpp

--- SpamCallDetector.cpp
+++ SpamCallDetector.cpp
@@ -1,139 +1,113 @@
 /*
 By: Caesar R. Watts-Hall
-Date: June 08, 2023
+Date: September 15, 2023
 */
 
-#include <iostream>
+// Include the necessary libraries for Qt and SQLite
+#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QWidget>
 #include <fstream>
 #include <string>
-#include <set>
+#include <map>
+#include <ctime>
+#include <sqlite3.h>
 using namespace std;
 
-set<string> blacklist;
-set<string> whitelist;
+// Define a map to store the spam criteria
+map<string, bool> spamCriteria;
 
-void loadLists() {
-    ifstream file;
+// Function to load the spam criteria from a file
+void loadSpamCriteria() {
+    // Open the file
+    ifstream file("spamCriteria.txt");
     string line;
-
-    file.open("blacklist.txt");
+    // Read each line from the file
     while (getline(file, line)) {
-        blacklist.insert(line);
+        // Ignore comment lines
+        if (line[0] == '#') continue;
+        // Extract the criterion and its value
+        string criterion = line.substr(0, line.find("="));
+        bool value = line.substr(line.find("=") + 1) == "true" ? true : false;
+        // Store the criterion in the map
+        spamCriteria[criterion] = value;
     }
-    file.close();
-
-    file.open("whitelist.txt");
-    while (getline(file, line)) {
-        whitelist.insert(line);
-    }
+    // Close the file
     file.close();
 }
 
+// Function to check if a phone number is spam based on the loaded criteria
 bool isSpamNumber(string phoneNumber) {
-    if (whitelist.count(phoneNumber) > 0) {
-        return false;
-    }
-    if (blacklist.count(phoneNumber) > 0) {
+    // If the number is in the whitelist, it's not spam
+    if (spamCriteria["startsWith9"] && phoneNumber[0] == '9') {
         return true;
     }
-
-    return phoneNumber[0] == '9';
+    // Add more criteria here...
+    return false;
 }
 
-void addToBlacklist(string phoneNumber) {
-    ofstream file;
-    file.open("blacklist.txt", ios::app);
-    file << phoneNumber << endl;
-    file.close();
+// Function to log each call to a SQLite database
+void logCall(string phoneNumber, bool isSpam) {
+    // Open the database
+    sqlite3* DB;
+    sqlite3_open("callLogs.db", &DB);
+
+    // Construct the SQL query to insert a new row into the table
+    string sql = "INSERT INTO CALL_LOGS (PHONE_NUMBER, TIME_STAMP, IS_SPAM) VALUES ('" 
+                 + phoneNumber + "', datetime('now'), " + (isSpam ? "1" : "0") + ");";
+
+    // Execute the SQL query
+    sqlite3_exec(DB, sql.c_str(), NULL, 0, NULL);
+
+    // Close the database
+    sqlite3_close(DB);
 }
 
-void removeFromBlacklist(string phoneNumber) {
-    ifstream file;
-    file.open("blacklist.txt");
-    ofstream temp;
-    temp.open("temp.txt");
-    string line;
-    while (getline(file, line)) {
-        if (line != phoneNumber) {
-            temp << line << endl;
+// The main function where the application starts
+int main(int argc, char **argv) {
+    // Create a Qt application object
+    QApplication app(argc, argv);
+
+    // Load the spam criteria from a file
+    loadSpamCriteria();
+
+    // Create a window widget and a layout for arranging child widgets
+    QWidget window;
+    QVBoxLayout layout(&window);
+
+    // Create a label widget for displaying text and add it to the layout
+    QLabel label("Enter phone number:");
+    layout.addWidget(&label);
+
+    // Create a line edit widget for entering text and add it to the layout
+    QLineEdit lineEdit;
+    layout.addWidget(&lineEdit);
+
+    // Create a push button widget and add it to the layout
+    QPushButton button("Check");
+    layout.addWidget(&button);
+
+    // Create another label widget for displaying the result and add it to the layout
+    QLabel result;
+    layout.addWidget(&result);
+
+   // Connect the button's clicked signal to a lambda function that checks if the entered number is spam and displays the result
+   QObject::connect(&button, &QPushButton::clicked, & {
+        QString phoneNumber = lineEdit.text();
+        bool isSpam = isSpamNumber(phoneNumber.toStdString());
+        if (isSpam) {
+            result.setText("The phone number is a spam number.");
+        } else {
+            result.setText("The phone number is not a spam number.");
         }
-    }
-    temp.close();
-    file.close();
-    remove("blacklist.txt");
-    rename("temp.txt", "blacklist.txt");
-}
+        logCall(phoneNumber.toStdString(), isSpam);
+   });
 
-void addToWhitelist(string phoneNumber) {
-    ofstream file;
-    file.open("whitelist.txt", ios::app);
-    file << phoneNumber << endl;
-    file.close();
-}
+   // Show the window and start the application's event loop
+   window.show();
 
-void removeFromWhitelist(string phoneNumber) {
-    ifstream file;
-    file.open("whitelist.txt");
-    ofstream temp;
-    temp.open("temp.txt");
-    string line;
-    while (getline(file, line)) {
-        if (line != phoneNumber) {
-            temp << line << endl;
-        }
-    }
-    temp.close();
-    file.close();
-    remove("whitelist.txt");
-    rename("temp.txt", "whitelist.txt");
-}
-
-void printMenu() {
-   cout << "Welcome to the Spam Caller Detector! Please select an option:" << endl;
-   cout << "[1] Spam Number Checker" << endl;
-   cout << "[2] Caller ID" << endl;
-   cout << "[3] Blacklist/Whitelist System" << endl;
-   cout << "[4] Reporting System" << endl;
-   cout << "[5] Exit/End Program" << endl;
-}
-
-int main() {
-   loadLists();
-
-   while (true) {
-       printMenu();
-       int choice;
-       cin >> choice;
-
-       if (choice == 1) {
-           string phoneNumber;
-           cout << "Enter phone number: ";
-           cin >> phoneNumber;
-
-           if (isSpamNumber(phoneNumber)) {
-               cout << "The phone number is a spam number." << endl;
-           } else {
-               cout << "The phone number is not a spam number." << endl;
-           }
-       } else if (choice == 2) {
-           // Caller ID
-           // This feature would likely require integration with your phone system or service provider,
-           // which is beyond the scope of a simple C++ program.
-       } else if (choice == 3) {
-           // Blacklist/Whitelist System
-           // Rest of the code...
-       } else if (choice == 4) {
-           // Reporting System
-           // This feature would likely require integration with your phone system or service provider,
-           // which is beyond the scope of a simple C++ program.
-       } else if (choice == 5) {
-           // Exit/End Program
-           cout << "Thank you for using this system. Have a nice day!" << endl;
-           break;
-       } else {
-           cout << "Invalid choice. Please try again." << endl;
-       }
-   }
-
-   return 0;
+   return app.exec();
 }
